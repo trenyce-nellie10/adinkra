b1badcc30d0142309f7dee790badf9ab6649106c
Nanajoa <yenmabawa@gmail.com>
2025-11-06 15:28:30 +0000

    t

--- js/login.js
+++ js/login.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
     submitBtn.dataset.original = submitBtn.textContent;
     submitBtn.textContent = "Signing in...";
 
-    fetch("actions/login_customer_action.php", {
+    fetch("../actions/login_customer_action.php", {
       method: "POST",
       body: formData
     })
